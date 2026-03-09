8ed24296f9fcb4abbb34c9441deeb1d458c92ab0
Developer <dev@atomity.com>
2026-03-09 13:27:52 +0530

    types: define typescript interfaces for Cluster, Namespace, Pod, and Metric data structures

---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+// src/types/index.ts
+
+export interface CostNode {
+  id:         string
+  label:      string
+  cpu:        number
+  ram:        number
+  storage:    number
+  network:    number
+  gpu:        number
+  efficiency: number
+  total:      number
+}
+
+export interface Pod extends CostNode {}
+
+export interface Namespace extends CostNode {
+  pods: Pod[]
+}
+
+export interface Cluster extends CostNode {
+  namespaces: Namespace[]
+}
+
+export type DrillLevel = 'cluster' | 'namespace' | 'pod'
+
+export interface BreadcrumbItem {
+  label: string
+  depth: number
+}
